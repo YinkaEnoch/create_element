557e818191fcc54014f2d32bdc7d7e165f77ddf1
yinkaenoch <adedokunyinkaenoch@gmail.com>
2020-08-16 03:58:38 +0100

    [test] add element class test

---
+++ __tests__/element_class.js
@@ -0,0 +1,26 @@
+const createNewElement = require("../src/index.ts");
+
+// Check if an element has a class attribute
+test("Element has a class attribute", () => {
+  let newElement = createNewElement({
+    elementType: "span",
+    elementClass: "red-text",
+  });
+
+  // First class property
+  let classProps = newElement.classList[0];
+
+  expect(classProps).toEqual("red-text");
+});
+
+// Check for multiple classes
+test("Element has a class attribute", () => {
+  let newElement = createNewElement({
+    elementType: "span",
+    elementClass: ["red-text", "blue-bg", "bold", "underline"],
+  });
+
+  expect(newElement.classList.toString()).toEqual(
+    "red-text blue-bg bold underline"
+  );
+});
